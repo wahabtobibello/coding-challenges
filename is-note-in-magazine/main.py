09d2db8586df1fe4165cabe4f4fd091c470f34b5
Tobi Bello <bello.tobi@outlook.com>
2018-10-14 19:32:03 +0100

    optimize code

--- is-note-in-magazine/main.py
+++ is-note-in-magazine/main.py
@@ -7,13 +7,16 @@ def is_source(note, magazine):
         else:
             note_word_count[word] = 1
             matched += 1
-    
+
     for word in magazine:
         if word in note_word_count:
             note_word_count[word] -= 1
             if note_word_count[word] == 0:
-                matched -= 0
-    
-    return matched == 0
-            
-        
+                matched -= 1
+            if matched == 0:
+                return True
+    return False
+
+
+print(is_source(['kill', 'you', 'i'],
+                ['the', 'fashion', 'kill', 'will', 'the', 'industry', 'i', 'love', 'this', 'you', 'need', 'this']))
