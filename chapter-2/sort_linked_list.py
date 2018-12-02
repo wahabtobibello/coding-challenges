d968711e76c717b5d18f288a4585aaef93118b8f
Tobi Bello <tobibello001@gmail.com>
2018-12-02 15:31:44 +0100

    added two new problems

---
+++ chapter-2/sort_linked_list.py
@@ -0,0 +1,5 @@
+def sort_linked_list(head):
+    '''
+        THe function should be able to take in a linked list and sort it
+    '''
+    pass
---
+++ chapter-2/weave_linked_list.py
@@ -0,0 +1,6 @@
+def weave_linked_list(head):
+    '''
+    Suppose you had a linked list a1->a2->...->an->b1->b2->...->bn.
+    You want to rearrange it into a a1->b1->a2->b2->...->an->bn                                                                                                                                              
+    '''
+    pass
