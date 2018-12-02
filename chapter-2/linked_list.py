eddac29b9c9a566f7d8498af5c4edfea2818c887
Tobi Bello <tobibello001@gmail.com>
2018-12-02 15:31:26 +0100

    chore: created linked list object

--- chapter-2/linked_list.py
+++ chapter-2/linked_list.py
@@ -3,28 +3,28 @@ class Node():
         self.data = data
         self.next = next
 
+
+class LinkedList():
+    def __init__(self, arr):
+        self.head = Node(arr[0])
+
+        node = self.head
+        for i in range(1, len(arr)):
+            end = Node(arr[i])
+            node.next = end
+            node = end
+
     def append_to_tail(self, data):
         end = Node(data)
-        node = self
+
+        node = self.head
         while node.next != None:
             node = node.next
         node.next = end
 
 
-def generate_linked_list(arr):
-    head = Node(arr[0])
-
-    node = head
-    for i in range(1, len(arr)):
-        end = Node(arr[i])
-        node.next = end
-        node = end
-
-    return head
-
-
-def print_linked_list(head):
-    node = head
+def print_linked_list(linked_list):
+    node = linked_list.head
     output = [str(node.data)]
     while node.next != None:
         output.append(str(node.next.data))
--- chapter-2/partition_linked_list.py
+++ chapter-2/partition_linked_list.py
@@ -1,10 +1,10 @@
-from linked_list import generate_linked_list, print_linked_list
+from linked_list import LinkedList, print_linked_list
 
 
-def partition_linked_list(head, partition):
-    new_head = head
+def partition_linked_list(linked_list, partition):
+    new_head = linked_list.head
 
-    node = head
+    node = new_head
     next_node = node.next
 
     while next_node != None:
@@ -16,11 +16,12 @@ def partition_linked_list(head, partition):
         else:
             node = next_node
             next_node = node.next
-    return new_head
+    
+    linked_list.head = new_head
 
 
 arr = [3, 5, 8, 5, 10, 2, 1]
-head = generate_linked_list(arr)
-print_linked_list(head)
-new_head = partition_linked_list(head, 5)
-print_linked_list(new_head)
+linked_list = LinkedList(arr)
+print_linked_list(linked_list)
+partition_linked_list(linked_list, 5)
+print_linked_list(linked_list)
